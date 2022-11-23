5fd40b97ee22a65db02d2b16e3da10007358b8ee
McRonaah <ronaldcheruiyot342@gmail.com>
2022-11-23 09:14:23 +0300

    CRUD operation updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :shops
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
 
   get "/pets", to: "pets#index"
+
+  # resource :pets, only: [:show, :create, :new, :edit, :update]
 end
